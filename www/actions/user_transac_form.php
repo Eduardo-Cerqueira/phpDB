dca167cf897f38feb25e9426b42b59d4b0a6db42
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-02-03 01:14:37 +0100

    transaction

--- www/actions/user_transac_form.php
+++ www/actions/user_transac_form.php
@@ -3,8 +3,8 @@ require_once __DIR__ . '/../../src/init.php';
 
 if ($_POST['type_transaction'] == 'transfer') {
     $dbManager->insert(
-        'INSERT INTO transfers(sender, receiver, amount, currency) VALUES(?, ?, ?, ?)',
-        [$user['IBAN'], $_POST['iban'], $_POST['amount'], $_POST['Currency']]
+        'INSERT INTO transfers(sender, receiver, amount, currency, created_by) VALUES(?, ?, ?, ?, ?)',
+        [$user['IBAN'], $_POST['iban'], $_POST['amount'], $_POST['Currency'], $user['id']]
     );}
 else {
     $dbManager->insert(
